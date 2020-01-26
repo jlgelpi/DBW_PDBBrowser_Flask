8d2546433c23ac232f9178557d4c75506df38dac
Josep Ll. Gelpi <gelpi@ub.edu>
2020-01-26 12:53:33 +0100

    Completed blast search

--- pdb_browser/__init__.py
+++ pdb_browser/__init__.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import re
+import uuid
+import subprocess
 
 from flask import Flask, render_template, session, request, url_for, redirect
 from flask_mysqldb import MySQL
@@ -73,13 +75,72 @@ def create_app(test_config=None):
         #PDB ID 
         if request.form['idCode']:
             return redirect(url_for('show', idCode=request.form['idCode']))
-        else:
+        elif request.form['seqQuery'] or 'seqFile' in request.files:
+            if request.files['seqFile'].filename:
+                sequence = request.files['seqFile'].read().decode('ascii')
+            else:
+                sequence = request.form['seqQuery']
         # Blast
+            session['query_seq'] = sequence
+            return redirect(url_for('blast'))
+        else:
         # Search
-        
             return request.form
 
 #
+# Blast
+#
+    @app.route('/blast/')
+    def blast():
+        query = session['query_seq']
+        if query[0] != '>': # Assumed not fasta
+            query = ">User provided sequence\n{}".format(query)
+        tmpFileName = os.path.join(app.instance_path, 'tmp/pdb') + str(uuid.uuid4())
+        query_fasta_file = tmpFileName + '.query_fasta'
+        blast_report_file = tmpFileName + '.blast_report'
+        with open(query_fasta_file, 'w') as query_fasta:
+            query_fasta.write(query)
+        subprocess.run(
+                app.config['BLAST_CMDLINE'] +\
+                ' -query {} -out {}'.format(query_fasta_file,blast_report_file),
+                shell=True
+        )
+        try:
+            results = []
+            with open(blast_report_file, 'r') as report:
+                for line in report:
+                    line = line.rstrip()
+                    if len(line) > 1: 
+                        id, header, ev = line.split("\t")
+                        m = re.match('(....)_(.) mol:([^ ]*) length:([0-9]*) *(.*)', header)
+                        results.append (
+                            {
+                                'idCode':m.group(1),
+                                'sub':m.group(2),
+                                'tip':m.group(3),
+                                'desc':m.group(5),
+                                'ev': ev
+                            }
+                        )
+                    
+        except IOError:
+            return render_template(
+                "error.html", 
+                title=app.config['TITLE'] + " No Blast results found", 
+                error_text='No results found. <p class="button" ><a href=\"\?new=1\">New Search</a></p>'
+            )
+        
+        os.remove(query_fasta_file)
+        os.remove(blast_report_file)
+        
+        return render_template(
+                'blast_output.html',
+                title=app.config['TITLE'] + " - Blast search",
+                count=len(results),
+                results=results
+        )
+        
+#   
 # Show Structure
 #
     @app.route('/show/<idCode>')
@@ -87,6 +148,12 @@ def create_app(test_config=None):
         glob_vars = globals()
         cur = mysql.connection.cursor()
         rs = cur.execute("SELECT e.* from entry e where e.idCode='{}'".format(idCode))
+        if not rs:
+            return render_template(
+                "error.html", 
+                title=app.config['TITLE'], 
+                error_text='Structure not found ({})'.format(idCode)
+            )
         columns = [col[0] for col in cur.description]
         data = dict(zip(columns, cur.fetchone()))
         rs = cur.execute("SELECT a.author from author a, author_has_entry ae where ae.idCode='{}' and a.idAuthor = ae.idAuthor order by a.author".format(idCode))
